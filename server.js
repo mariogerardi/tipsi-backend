d1b6e0fad0e7503688729621328f44997f67bbba
mariogerardi <mgerardi@vassar.edu>
2022-03-10 12:30:50 -0500

    Still troubleshooting.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 
 const mongoose = require("mongoose");
 
