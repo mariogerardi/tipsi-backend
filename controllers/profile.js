8126efd0c7247bd22a22fb14083018396c0e38ab
mariogerardi <mgerardi@vassar.edu>
2022-03-14 21:34:58 -0400

    Trying out a putroute change.

--- controllers/profile.js
+++ controllers/profile.js
@@ -32,7 +32,7 @@ router.get("/:username", async (req, res) => {
 router.put("/:id", async (req, res) => {
   try {
       res.json(
-          await Profile.findByIdAndUpdate(req.params.id, req.body, { new: true })
+          await Profile.findByIdAndUpdate({_id: req.params.id}, req.body)
       );
   } catch (error) {
       res.status(400).json(error);
